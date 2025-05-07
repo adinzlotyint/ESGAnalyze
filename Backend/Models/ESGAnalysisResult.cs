d4a2f0a9c79ae4fbad5a02f7902bce4c8529dbb7
adinzlotyint <adrian.kamil.zietek@gmail.com>
2025-05-07 17:43:41 +0200

    Changed results model to represent numerical values instead of bool

--- Backend/Models/ESGAnalysisResult.cs
+++ Backend/Models/ESGAnalysisResult.cs
@@ -1,21 +1,30 @@
 ﻿namespace ESGanalyzer.Backend.Models {
     public class ESGAnalysisResult {
-        public bool HasScopeEmissions { get; set; }
-        public bool HasStandardReferences { get; set; }
-        public bool HasNumericConsistency { get; set; }
-        public bool HasReductionTargets { get; set; }
-        public bool HasClimateRiskDiscussion { get; set; }
-        public bool HasEfficiencyIndicators { get; set; }
+        public double C1PolicyStrategyScore { get; set; }          // [0, .5, 1]
+        public double C2RiskOpportunityScore { get; set; }         // [0, .33, .67, 1]
+        public double C3GovernanceStructureScore { get; set; }     // [0, .5, 1]
+        public double C4EmissionsScopeScore { get; set; }          // [0, .25, .5, .75, 1]
+        public double C5EmissionsBoundaryScore { get; set; }       // [0, .33, .67, 1]
+        public double C6CalculationStandardScore { get; set; }     // [0, 1]
+        public double C7GwpSourcesScore { get; set; }              // [0, 1]
+        public double C8EmissionsTrendScore { get; set; }          // [0, .5, 1]
+        public double C9IntensityIndicatorScore { get; set; }      // [0, 1]
+        public double C10ReductionTargetsScore { get; set; }       // [0, .33, .5, .67, 1]
+        public double C11UnitCorrectnessScore { get; set; }        // [0, 1]
+        public double C12KeywordPresenceScore { get; set; }        // [0, 1]
 
-        public int GetTotalScore() {
-            int score = 0;
-            if (HasScopeEmissions) score++;
-            if (HasStandardReferences) score++;
-            if (HasNumericConsistency) score++;
-            if (HasReductionTargets) score++;
-            if (HasClimateRiskDiscussion) score++;
-            if (HasEfficiencyIndicators) score++;
-            return score;
+        public double GetTotalScore() {
+            return
+                C1PolicyStrategyScore +
+                C2RiskOpportunityScore +
+                C3GovernanceStructureScore +
+                C4EmissionsScopeScore +
+                C5EmissionsBoundaryScore +
+                C6CalculationStandardScore +
+                C7GwpSourcesScore +
+                C8EmissionsTrendScore +
+                C9IntensityIndicatorScore +
+                C10ReductionTargetsScore;
         }
     }
 }
