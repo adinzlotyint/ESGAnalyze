b2abd803f2e58512ab16adda7011981f95ccd8c3
Adrian Ziętek <adrian.kamil.zietek@gmail.com>
2025-07-19 19:48:08 +0200

    test(parse): Add WithValidInput and WithValidEmptyPdf tests

--- Tests/ESGAnalyzer.Tests.Unit/ParseServiceTests.cs
+++ Tests/ESGAnalyzer.Tests.Unit/ParseServiceTests.cs
@@ -7,9 +7,15 @@ using System.Threading.Tasks;
 using ESGanalyzer.API.Services;
 using FluentAssertions;
 using ESGanalyzer.API.Exceptions;
+using Xunit.Abstractions;
 
 namespace ESGAnalyzer.Tests.Unit {
     public class ParseServiceTests {
+        private readonly ITestOutputHelper _output;
+        public ParseServiceTests(ITestOutputHelper output) {
+            _output = output;
+        }
+
         [Theory]
         [InlineData(null)]
         public async Task ExtractTextFromPDFAsync_WithInvalidInput_ReturnsException(Stream fileContent) {
@@ -20,5 +26,33 @@ namespace ESGAnalyzer.Tests.Unit {
             //Assert
             await act.Should().ThrowAsync<ParsingFailedException>();
         }
+
+        [Fact]
+        public async Task ExtractTextFromPDFAsync_WithValidInput_ReturnsString() {
+            //Arrange
+            var parseService = new ParseService();
+            var filePath = Path.Combine(AppContext.BaseDirectory, "TestAssets", "Valid-document.pdf");
+            await using var fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+            //Act
+            var result = await parseService.ExtractTextFromPDFAsync(fileStream);
+            //Assert
+            result.Should().BeOfType<String>();
+            result.Should().Be("Valid pdf");
+        }
+
+        [Fact]
+        public async Task ExtractTextFromPDFAsync_WithValidEmptyPdf_ReturnsEmptyString() {
+            //Arrange
+            var parseService = new ParseService();
+            var filePath = Path.Combine(AppContext.BaseDirectory, "TestAssets", "Empty-document.pdf");
+            await using var fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+            //Act
+            var result = await parseService.ExtractTextFromPDFAsync(fileStream);
+
+            // Assert
+            result.Should().BeEmpty();
+        }
     }
+
+
 }
