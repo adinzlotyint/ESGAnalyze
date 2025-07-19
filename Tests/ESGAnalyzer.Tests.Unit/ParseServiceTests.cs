15bcd729ad6cd8e875956688a3198ca4429eb6b0
Adrian Ziętek <adrian.kamil.zietek@gmail.com>
2025-07-19 18:18:00 +0200

    test(parse): Add first test with invalid input

---
+++ Tests/ESGAnalyzer.Tests.Unit/ParseServiceTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ESGanalyzer.API.Services;
+using FluentAssertions;
+using ESGanalyzer.API.Exceptions;
+
+namespace ESGAnalyzer.Tests.Unit {
+    public class ParseServiceTests {
+        [Theory]
+        [InlineData(null)]
+        public async Task ExtractTextFromPDFAsync_WithInvalidInput_ReturnsException(Stream fileContent) {
+            //Arrange
+            var parseService = new ParseService();
+            //Act
+            var act = () => parseService.ExtractTextFromPDFAsync(fileContent);
+            //Assert
+            await act.Should().ThrowAsync<ParsingFailedException>();
+        }
+    }
+}
