d1318d660affeec182e318672a6367c85514c402
adinzlotyint <adrian.kamil.zietek@gmail.com>
2025-07-31 20:00:05 +0200

    feat: replace training with jupyter notebook

--- definitions.py
+++
@@ -1,8 +0,0 @@
-import os
-import json
-
-ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-BASE_MODEL_PATH = os.path.join(ROOT_DIR, 'base_model/')
-
-with open(f"{ROOT_DIR}/config.json", 'r') as config:
-  CONFIG = json.load(config)
